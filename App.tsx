30d4d02178c4acbdf7c69a578241dfd9570fe767
Sean Behan <codebam@riseup.net>
2023-10-22 05:14:19 -0400

    use react native paper

--- App.tsx
+++ App.tsx
@@ -4,12 +4,30 @@ import {
 	StyleSheet,
 	View,
 	TextInput,
-	Button,
 	KeyboardAvoidingView,
 } from "react-native";
+import { PaperProvider, Button, MD3DarkTheme } from "react-native-paper";
 import MessageView from "./src/MessageView";
+import { useTheme } from "react-native-paper";
 
 export default function App() {
+	const theme = useTheme();
+	const styles = StyleSheet.create({
+		input: {
+			height: "40%",
+			margin: "5%",
+			borderWidth: 1,
+			padding: 10,
+			backgroundColor: theme.colors.secondary,
+			width: 250,
+		},
+		container: {
+			flex: 1,
+			alignItems: "center",
+			justifyContent: "center",
+			backgroundColor: theme.colors.primary,
+		},
+	});
 	const [text, setText] = useState("");
 	const [messages, setMessages] = useState([
 		"example message",
@@ -21,36 +39,27 @@ export default function App() {
 		"why are these here",
 	]);
 	return (
-		<View style={styles.container}>
-			<MessageView messages={messages} />
-			<KeyboardAvoidingView
-				style={{
-					flexDirection: "row",
-					width: "100%",
-					alignItems: "center",
-					justifyContent: "center",
-				}}
-				behavior={"padding"}
-			>
-				<TextInput style={styles.input} value={text} onChangeText={setText} />
-				<Button title={"Send"} />
-			</KeyboardAvoidingView>
-			<StatusBar style="auto" />
-		</View>
+		<PaperProvider
+			theme={{
+				...MD3DarkTheme,
+			}}
+		>
+			<View style={styles.container}>
+				<MessageView messages={messages} />
+				<KeyboardAvoidingView
+					style={{
+						flexDirection: "row",
+						width: "100%",
+						alignItems: "center",
+						justifyContent: "center",
+					}}
+					behavior={"padding"}
+				>
+					<TextInput style={styles.input} value={text} onChangeText={setText} />
+					<Button icon="send">send</Button>
+				</KeyboardAvoidingView>
+				<StatusBar style="auto" />
+			</View>
+		</PaperProvider>
 	);
 }
-
-const styles = StyleSheet.create({
-	input: {
-		height: "40%",
-		margin: "5%",
-		borderWidth: 1,
-		padding: 10,
-		paddingRight: "55%",
-	},
-	container: {
-		flex: 1,
-		alignItems: "center",
-		justifyContent: "center",
-	},
-});
